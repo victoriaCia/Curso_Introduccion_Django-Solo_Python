dfed00e24f7b6285e564c7c2e55fe0a4e8758381
victoriaCia <107944947+victoriaCia@users.noreply.github.com>
2022-08-05 13:57:02 -0300

    modified:   blog/forms.py 	modified:   blog/urls.py 	modified:   blog/views.py 	modified:   core/settings.py 	modified:   requirements.txt 	new file:   templates/base.html 	modified:   templates/blog_create.html 	new file:   templates/blog_delete.html 	new file:   templates/blog_detail.html 	modified:   templates/blog_list.html 	new file:   templates/blog_update.html 	modified:   templates/index.html 	new file:   theme/__init__.py 	new file:   theme/apps.py 	new file:   theme/static_src/.gitignore 	new file:   theme/static_src/package-lock.json 	new file:   theme/static_src/package.json 	new file:   theme/static_src/postcss.config.js 	new file:   theme/static_src/src/styles.css 	new file:   theme/static_src/tailwind.config.js 	new file:   theme/templates/base.html

--- core/settings.py
+++ core/settings.py
@@ -36,7 +36,19 @@ INSTALLED_APPS = [
 
     'core',
     'blog',
+
+    'tailwind',
+    'theme',
 ]
+
+TAILWIND_APP_NAME = 'theme'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+
+NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
